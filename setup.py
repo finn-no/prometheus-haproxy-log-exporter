9f0ad9dfe2aed1c53ebb32c7711ad2ddb0f458de
fimojoha <morten.lied.johansen@finn.no>
2016-06-11 23:14:25 +0200

    Work around stupid debian tools

--- setup.py
+++ setup.py
@@ -1,6 +1,26 @@
 #!/usr/bin/env python3
 
+import os
+
 from setuptools import setup, find_packages
+
+# When making a debian package from this package, inline requirements just confuses the debian
+# tools, so simply leave them out if the DH_INTERNAL_BUILDFLAGS env-variable is set.
+if os.getenv("DH_INTERNAL_BUILDFLAGS"):
+    KW_ARGS={}
+else:
+    KW_ARGS={
+        "install_requires":[
+             'configargparse',
+             'prometheus-client',
+             #'systemd', ??? Unknown which module this is
+             'haproxy-log-analysis<2.0',
+             #'pkg-resources', ??? Unknown which module this is
+             'tailhead'
+        ],
+        "setup_requires":['pytest-runner'],
+        "tests_require":['pytest-sugar', 'pytest-html', 'pytest-cov', 'pytest'],
+    }
 
 setup(
     name="prometheus-haproxy-log-exporter",
@@ -10,15 +30,5 @@ setup(
             'prometheus-haproxy-log-exporter = prometheus_haproxy_log_exporter.cli:main',
         ],
     },
-    # Requirements
-    install_requires=[
-         'configargparse',
-         'prometheus-client',
-         #'systemd', ??? Unknown which module this is
-         'haproxy-log-analysis<2.0',
-         #'pkg-resources', ??? Unknown which module this is
-         'tailhead'
-    ],
-    setup_requires=['pytest-runner'],
-    tests_require=['pytest-sugar', 'pytest-html', 'pytest-cov', 'pytest'],
+    **KW_ARGS
 )
